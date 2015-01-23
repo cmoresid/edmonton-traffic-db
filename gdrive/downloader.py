8c052a31d3b6a53e45129c32bd82f93b6cb12bc1
Connor Moreside <cmoresid@ualberta.ca>
2015-01-22 23:26:45 -0700

    Downloading works

--- gdrive/downloader.py
+++ gdrive/downloader.py
@@ -20,19 +20,17 @@ class GDriveDownloader():
         """
             Borrowed from: https://developers.google.com/drive/v2/reference/files/get
         """       
-        file_destination = os.path.join(self.output_dir, drive_file.get('title'))
+        file_destination = os.path.join(self.output_dir, drive_file.get('originalFilename'))
         file_id = drive_file.get('id')
         
         if os.path.exists(file_destination):
-            return GDriveDownloadResult(file_id, False, "%s exist; do not download." % (file_destination,))
+            return GDriveDownloadResult(file_id, False, "%s already exists." % (file_destination,))
         
         download_url = drive_file.get('downloadUrl')
         if download_url:
             resp, content = self._service._http.request(download_url)
             
-            if resp.status == 200:
-                file_destination = os.path.join(self.output_dir, drive_file.get('title'))
-                
+            if resp.status == 200:                
                 with open(file_destination, 'w+') as output_file:
                     output_file.write(content)
                     
@@ -43,21 +41,32 @@ class GDriveDownloader():
             # The file doesn't have any content stored on Drive.
             return GDriveDownloadResult(file_id, False, 'No content stored on Drive.')
             
+    def download_file_by_id(self, file_id):
+        drive_file = self._service.files().get(fileId=file_id).execute()
+        
+        return self.download_file(drive_file)
+            
     def download_files_in_folder(self, folder_id):
-        drive_folder = self._service.files.list(q="'%s' in parents" % (folder_id,), 
-                                                fields='items(id,downloadUrl,originalFilename),nextPageToken')
+        drive_folder = self._service.files().list(q="'%s' in parents" % (folder_id,), 
+                                                fields='items(id,downloadUrl,originalFilename),nextPageToken').execute()
         files = drive_folder.get('items', [])
         
         return map(self.download_file, files)
         
-    
-            
-            
-    
-
-            
-    
+    def display_results(self, download_results):
+        downloaded = filter(lambda result: result.success, download_results)
+        not_downloaded = filter(lambda result: not(result.success), download_results)
         
-    
-    
+        print '----------------------------------------------------'
+        print 'Successfully Downloaded'
+        print '----------------------------------------------------'
+        
+        for gresult in downloaded:
+            print '%s - Downloaded to %s' % (gresult.file_id,gresult.result)
             
+        print '----------------------------------------------------'
+        print 'Not Downloaded'
+        print '----------------------------------------------------'
+        
+        for gresult in not_downloaded:
+            print '%s - Not downloaded because "%s"' % (gresult.file_id, gresult.result)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from processor.traffic_data_import import TrafficDataDBImport
+import os
 
 if __name__ == "__main__":
     importer = TrafficDataDBImport()
     
-    importer.traffic_data_import(['data/ABRoad.xls'])
+    spreadsheets = [os.path.join('data', f) for f in os.listdir('data') if os.path.join('data',f).endswith('.xls') ]
+    importer.traffic_data_import(spreadsheets)
     
